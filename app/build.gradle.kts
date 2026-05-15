24596ffc14bf2ea676b83cf07dd6ce740f820471
Mirurin <MiruJam@outlook.com>
2026-05-16 02:32:48 +0800

    build: bump app version to 1.1 (2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "mirujam.nekomemo"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

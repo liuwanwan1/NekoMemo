22411f27a96f91bc63018bdd80e2973e4ddcc0b1
Mirurin <MiruJam@outlook.com>
2026-05-17 17:59:53 +0800

    build: remove release signing configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,8 @@
-import java.util.Properties
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt.android)
-}
-
-val localProperties = Properties().apply {
-    val file = rootProject.file("local.properties")
-    if (file.exists()) {
-        load(file.inputStream())
-    }
 }
 
 android {
@@ -28,21 +19,8 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    signingConfigs {
-        create("release") {
-            val path = localProperties.getProperty("RELEASE_STORE_FILE")
-            if (path != null) {
-                storeFile = file(path)
-                storePassword = localProperties.getProperty("RELEASE_STORE_PASSWORD")
-                keyAlias = localProperties.getProperty("RELEASE_KEY_ALIAS")
-                keyPassword = localProperties.getProperty("RELEASE_KEY_PASSWORD")
-            }
-        }
-    }
-
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
@@ -50,11 +28,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-
-        debug {
-            signingConfig = signingConfigs.getByName("release")
-        }
-
     }
 
     compileOptions {
